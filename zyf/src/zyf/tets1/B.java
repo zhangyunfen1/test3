83902d51a9e356cc450f6a1be098d8195f7e38a3
zhangyunfen <2864601547@qq.com>
2020-03-22 18:55:09 +0800

    11

--- zyf/src/zyf/tets1/B.java
+++ zyf/src/zyf/tets1/B.java
@@ -1,8 +1,18 @@
 package zyf.tets1;
 
+import java.util.TreeSet;
+
 public class B {
+	
 	public static void main(String[] args) {
-		Integer.valueOf(2).intValue();
 		
+		TreeSet<Integer> set = new TreeSet<Integer>();
+		TreeSet<Integer> subset = new TreeSet<Integer>();
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		TreeSet t = (TreeSet)set.subSet(1, true,4,false);	
+		System.out.println(t);
 	}
 }
